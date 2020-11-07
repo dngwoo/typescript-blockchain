bc6d57764d973b37c6eabc947fbfc33d28597f25
dngwoo <akfmzk84@naver.com>
2020-11-07 20:32:06 +0900

    Blockchain Creating a Block

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,28 @@
-//
-
-// 클래스를 사용하는 이유는 인터페이스를 dist/index.js에 넣고 싶을 때 사용한다.
-class Human {
-  name: string;
-  private age: number;
-  gender: string;
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  index: number;
+  hash: string;
+  previousHash: string;
+  data: string;
+  timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const person = {
-  name: 'dngwoo',
-  gender: 'male',
-  age: 22,
-};
+const genesisBlock: Block = new Block(0, '202020202020', '', 'Hello', 123456);
 
-const sayHi = (person: Human): void => {
-  console.log(
-    `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`
-  );
-};
+const blockchain: [Block] = [genesisBlock];
 
-sayHi(person); // age에서 에러가 뜨게 된다. 클래스에서 private을 쓰면 dist/index.js에서는 존재하지 않는다.
+console.log(blockchain);
+
+blockchain.push('stuff'); // 타입이 Block이 아니기 때문에 에러가 난다.
