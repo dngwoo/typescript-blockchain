7aae596fb6d5df62d78d405a042ea0cd4662c660
dngwoo <akfmzk84@naver.com>
2020-11-07 20:25:27 +0900

    ts에서 클래스 private 사용해보기

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 //
 
+// 클래스를 사용하는 이유는 인터페이스를 dist/index.js에 넣고 싶을 때 사용한다.
 class Human {
   name: string;
-  age: number;
+  private age: number;
   gender: string;
   constructor(name: string, age: number, gender: string) {
     this.name = name;
@@ -23,4 +24,4 @@ const sayHi = (person: Human): void => {
   );
 };
 
-sayHi(person);
+sayHi(person); // age에서 에러가 뜨게 된다. 클래스에서 private을 쓰면 dist/index.js에서는 존재하지 않는다.
