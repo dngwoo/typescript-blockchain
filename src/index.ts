8e5ebf54f66f9b4409257cf0b41bd7562cf58846
dngwoo <akfmzk84@naver.com>
2020-11-07 20:02:01 +0900

    인터페이스 사용해보기

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
-const myName = 'dngwoo';
-const age = 24;
-const gender = 'male';
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
 
-const sayHi = (myName: string, age: number, gender: string): void => {
-  // 여기에 ?을 써주면 옵셔널 파라미터가 된다.
-  console.log(`Hello ${myName}, you are ${age}, you are a ${gender}!`);
+const person = {
+  name: 'dngwoo',
+  gender: 'male',
+  age: 22,
 };
 
-sayHi(myName, age, gender); // 이제 이 부분은 에러가 나지 않는다.
+const sayHi = (person: Human): void => {
+  console.log(
+    `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`
+  );
+};
+
+sayHi(person);
