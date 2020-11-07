3c9363da038e9e30c380c553399abcd846179fa5
dngwoo <akfmzk84@naver.com>
2020-11-07 19:30:39 +0900

    옵셔널 파라미터를 쓰는 이유

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log('hello');
+const myName = 'dngwoo';
+const age = 24;
+const gender = 'male';
+
+const sayHi = (myName, age, gender) => {
+  console.log(`Hello ${myName}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(myName, age); // 이 부분 때문에 에러가 난다.
